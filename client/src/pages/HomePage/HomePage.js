481389018aee01142077f03c63e13fb42eb4a583
JekIquin <jci.dev.0@gmail.com>
2021-09-06 16:41:09 -0700

    added jojo on homepage

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -6,6 +6,7 @@ const HomePage = () => {
         <main className="homepage">
             {/* <img src="http://localhost:6969/logo/Logo.png" alt="logo" /> */}
             <h1 className="homepage__heading">Jojo's Adventure</h1>
+            <div className="homepage__jojo"></div>
             <p>Whatever this is....</p>
             <Link className="homepage__start" to='/story'> 
                 <p className="homepage__start-button">START GAME</p>
