e5721dc29b81c2618ad2c4881144c9d37ecb8d0d
JekIquin <jci.dev.0@gmail.com>
2021-09-06 17:05:31 -0700

    randomizer for punch and jump

--- client/src/components/Animation/Animation.js
+++ client/src/components/Animation/Animation.js
@@ -4,13 +4,27 @@ function Animation(props) {
     const {image, animate, isDead, isEnd} = props;
     // image = image || 'http://localhost:6969/images/boss.png';
     // console.log(props.storyLine);
+    const randomizer = Math.floor(Math.random()*2);
+    const animationClasses = (class1, class2, class3=null) => {
+        let className = animate ? class1 : class2;
+        if (!class3 && animate) {
+            return className;
+        }
+        return (image?.includes('boss') || randomizer) && animate ? class3 : className;
 
-    const animateEnv = animate ? 'animation--run' : '';
-    let animateJojo = animate ? 'animation__jojo--run' : 'animation__jojo--standing';
-    animateJojo = image?.includes('boss') ? 'animation__jojo--punch' : animateJojo;
-    let animateObstruction = animate ? 'animation__obstruction--run' : '';
-    animateObstruction = image?.includes('boss') ? 'animation__obstruction--punch' : animateObstruction;
-    let overMessage = isEnd ? (isDead ? 'GAME OVER!' : 'WINNER!!!!') : '';
+    }
+
+        const animateEnv = animationClasses('animation--run', '');
+        const animateJojo = animationClasses('animation__jojo--run', 'animation__jojo--standing', 'animation__jojo--punch');
+        const animateObstruction = animationClasses('animation__obstruction--run', '', 'animation__obstruction--punch');
+        let overMessage = isEnd ? (isDead ? 'GAME OVER!' : 'WINNER!!!!') : '';
+
+    // const animateEnv = animate ? 'animation--run' : '';
+    // let animateJojo = animate ? 'animation__jojo--run' : 'animation__jojo--standing';
+    // animateJojo = image?.includes('boss') ? 'animation__jojo--punch' : animateJojo;
+    // let animateObstruction = animate ? 'animation__obstruction--run' : '';
+    // animateObstruction = image?.includes('boss') ? 'animation__obstruction--punch' : animateObstruction;
+    
     
 
     return(
